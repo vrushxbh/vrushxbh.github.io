2d3650dbb36ba05ae07ceb693dbd034cd8930739
vrtumma <anivs@VRTUMMA-IN.in.oracle.com>
2022-03-13 16:17:46 +0530

    BR second commit

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const video = document.querySelector("video")
 const textElement = document.querySelector("[data-text]")
+const playButton = document.getElementById("play-button")
+const pauseButton = document.getElementById("pause-button")
+const stopButton = document.getElementById("stop-button")
+const speedInput = document.getElementById("speed")
+const textInput = document.getElementById("text")
+let currentCharacter
 
 async function setup() {
     const stream = await navigator.mediaDevices.getUserMedia({ video: true })
@@ -21,13 +27,54 @@ async function setup() {
             const {
                  data: { text },
             } = await worker.recognize(canvas)
-
-            speechSynthesis.speak(new SpeechSynthesisUtterance(text.replace(/\s/g, " ")))
+            textInput.disabled = false
+            //speechSynthesis.speak(new SpeechSynthesisUtterance(text.replace(/\s/g, " ")))
 
             textElement.textContent = text
+
+            playButton.addEventListener("click", () => {
+                playText(textInput.value.replace(/\s/g, " "))
+            })
+
+            pauseButton.addEventListener("click", pauseText)
+            stopButton.addEventListener("click", stopText)
+            speedInput.addEventListener("input", () => {
+                stopText()
+                playText(utterence.text.substring(currentCharacter))
+            })
         })
+
+        
         
     })
+}
+
+const utterence = new SpeechSynthesisUtterance()
+utterence.addEventListener("end", () => {
+    textInput.disabled = false
+})
+utterence.addEventListener("boundary", e => {
+    currentCharacter = e.charIndex
+})
+
+function playText(text) {
+    
+    if (speechSynthesis.paused && speechSynthesis.speaking) {
+        return speechSynthesis.resume()
+    }
+    utterence.text = text
+    utterence.rate = speedInput.value || 1
+    textInput.disabled = true
+    speechSynthesis.speak(utterence)
+}
+
+function pauseText() {
+    if(speechSynthesis.speaking) speechSynthesis.pause()
+}
+
+function stopText() {
+    speechSynthesis.resume()
+    speechSynthesis.cancel()
 }
 
 setup()
